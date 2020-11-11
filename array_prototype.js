df99b8a70697b8edb24afa9745baadb267f2d6e0
Guilherme <jogos.coisas@gmail.com>
2020-11-11 17:45:45 -0300

    without property - bug fix

--- array_prototype.js
+++ array_prototype.js
@@ -39,41 +39,63 @@ Array.prototype.distinctField = function (field) {
 }
 
 Array.prototype.groupBy = function (field) {
+  return _groupBy(field, this);
+}
+
+function _groupBy(field, array) {
   let result = [];
-  for (var i = 0; i < this.length; i++) {
-    if (this[i].hasOwnProperty(field)) {
-      let obj = {
-        field: null,
-        itens: []
-      }
+  for (var i = 0; i < array.length; i++) {
+
+    let obj = {
+      field: null,
+      itens: [],
+    }
+    if (array[i].hasOwnProperty(field)) {
       //exist
       for (var ii = 0; ii < result.length; ii++) {
-        if (result[ii].field == this[i][field]) {
+        if (result[ii].field == array[i][field]) {
           obj = result[ii];
           break;
         }
       }
-      if (!obj.field) {
-        obj.field = this[i][field];
-        obj.itens.push(this[i]);
+      if (!obj.field && obj.field != "") {
+        obj.field = array[i][field];
+        obj.itens.push(array[i]);
         result.push(obj);
       }
       else
-        obj.itens.push(this[i]);
+        obj.itens.push(array[i]);
+    } else {
+      let finded = false;
+      for (let index = 0; index < result.length; index++) {
+        if (result[index].field == '' ) {
+            result[index].itens.push(array[i])
+            finded = true;
+            break;
+        }
+      }
+      if (!finded) {
+        obj = {
+          field: '',
+          itens: [array[i]]
+        };
+        result.push(obj);
+      }
     }
   }
   return result;
 }
 
 let array = [
-  { a: 'a', b: 1 },
-  { a: 'a', b: 1 },
-  { a: 'a', b: 1 },
-  { a: 'b', b: 1 },
-  { a: 'c', b: 1 }
+  { field1: 'a', field2: 1 },
+  { field1: 'a', field2: 1 },
+  { field1: 'a', field2: 1 },
+  { field1: 'b', field2: 1 },
+  { field1: 'c', field2: 1 },
+  { field2: 1 }
 ];
 
 console.log('original array', array);
 console.log('distinct', array.distinct());
-console.log('distinctField', array.distinctField('a'));
-console.log('groupBy', array.groupBy('a'));
+console.log('distinctField', array.distinctField('field1'));
+console.log('groupBy', array.groupBy('field1'));
